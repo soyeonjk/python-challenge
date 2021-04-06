26f639a482d90bde47a1d4bfcd01b2396cbf0c88
soyeonjk <80230842+soyeonjk@users.noreply.github.com>
2021-04-06 00:50:28 -0400

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,51 @@
+import os
+import csv
+file = os.path.join('..', 'Resources', 'budget_data.csv')
+with open(file) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter = ',')
+    header = next(csvreader)
+    #create empty lists to add the csv values to 
+    month_count = []
+    profit = []
+    change_profit = []
+    
+                      
+    #iterate through the values and add them to the empty list 
+    for row in csvreader:
+        month_count.append(row[0])
+        profit.append(int(row[1]))
+    for i in range(len(profit)-1):
+        change_profit.append(profit[i+1]-profit[i])
+                      
+#evaluate the max and min from the list made
+increase = max(change_profit)
+decrease = min(change_profit)
 
+#using the index
+month_increase = change_profit.index(max(change_profit))+1
+month_decrease = change_profit.index(min(change_profit))+1
+
+
+print("Financial Analysis")
+print("------------------------")
+print(f"Total Months:{len(month_count)}")
+print(f"Total: ${sum(profit)}")
+print(f"Average Change: {round(sum(change_profit)/len(change_profit),2)}")
+print(f"Greatest Increase in Profits: {month_count[month_increase]} (${(str(increase))})")
+print(f"Greatest Decrease in Profits: {month_count[month_decrease]} (${(str(decrease))})")      
+
+
+with open('financial_analysis.txt', "w") as new:
+    new.write("Financial Analysis")
+    new.write("\n")
+    new.write("------------------------")
+    new.write("\n")
+    new.write(f"Total Months:{len(month_count)}")
+    new.write("\n")
+    new.write(f"Total: ${sum(profit)}")
+    new.write("\n")
+    new.write(f"Average Change: {round(sum(change_profit)/len(change_profit),2)}")
+    new.write("\n")
+    new.write(f"Greatest Increase in Profits: {month_count[month_increase]} (${(str(increase))})")
+    new.write("\n")
+    new.write(f"Greatest Decrease in Profits: {month_count[month_decrease]} (${(str(decrease))})")
